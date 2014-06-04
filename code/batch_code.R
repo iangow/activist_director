1354cc7e7a16de8de9d29af4d063a2a1d7e6baa4
Ian Gow <igow@hbs.edu>
2014-06-04 11:21:14 -0400

    Altered code to remove circularity between activism_events and key_dates.

--- code/batch_code.R
+++ code/batch_code.R
@@ -7,6 +7,8 @@ rs <- dbGetQuery(pg, "
     CREATE SCHEMA IF NOT EXISTS activist_director")
 
 rs <- dbGetQuery(pg, "
+    DROP AGGREGATE IF EXISTS product(double precision);
+
     CREATE AGGREGATE product(double precision) (
         SFUNC=float8mul,
         STYPE=float8
@@ -30,29 +32,44 @@ pg_comment <- function(table, comment) {
     dbDisconnect(pg)
 }
 
+source("code/import_activist_directors.R")
 source("code/import_activist_ciks.R")
-runSQL("code/create_activist_holdings.sql")
-
-runSQL("code/create_view_financials.sql")
-pg_comment("activist_director.financials",
-           "CREATED USING create_view_financials.sql")
 
 # Column 5
 source('code/create_activism_events.R', echo=TRUE)
 
-# This one takes a long time to run!
-source("code/create_activist_holdings_matched_ss.R", echo=TRUE)
-
-source('code/create_equilar_w_activism.R', echo=TRUE)
+source("code/import_key_dates.R")
 
 runSQL("code/create_activist_director_matched.sql")
 pg_comment("activist_director.activist_director_matched",
            "CREATED USING activist_director_matched.sql")
 
 
+runSQL("code/create_activist_holdings.sql")
+
 runSQL('code/create_equilar_directors.sql')
 pg_comment("activist_director.equilar_directors",
            "CREATED USING create_equilar_directors.sql")
+
+source('code/create_equilar_w_activism.R', echo=TRUE)
+
+
+
+
+
+runSQL("code/create_view_financials.sql")
+pg_comment("activist_director.financials",
+           "CREATED USING create_view_financials.sql")
+
+
+
+# This one takes a long time to run!
+source("code/create_activist_holdings_matched_ss.R", echo=TRUE)
+
+
+
+
+
 
 runSQL('code/create_first_voting.sql')
 
