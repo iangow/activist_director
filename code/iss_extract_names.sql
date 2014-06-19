d8ef80f60be6142f322788bb21d1d88c3740f249
Ian Gow <igow@hbs.edu>
2014-06-19 14:11:25 -0400

    Uncommented CREATE TYPE code.
    This generates an error (without harm) if the type already exists.

--- code/iss_extract_names.sql
+++ code/iss_extract_names.sql
@@ -1,19 +1,19 @@
-﻿-- DROP TYPE IF EXISTS parsed_name CASCADE;
--- 
--- CREATE TYPE parsed_name AS
---    (prefix text,
---    first_name text,
---     middle_initial text,
---     last_name text,
---     suffix text);
--- 
+-- DROP TYPE IF EXISTS parsed_name CASCADE;
+--
+ CREATE TYPE parsed_name AS
+    (prefix text,
+    first_name text,
+     middle_initial text,
+     last_name text,
+     suffix text);
+--
 -- ALTER TYPE parsed_name OWNER TO personality_access;
 
 DROP FUNCTION IF EXISTS issvoting.extract_name(text);
 
 CREATE OR REPLACE FUNCTION issvoting.extract_name(text)
-RETURNS parsed_name AS 
-$CODE$       
+RETURNS parsed_name AS
+$CODE$
     use utf8;
     $first_name="";
     $last_name="";
@@ -25,7 +25,7 @@ $CODE$
     if (defined($_[0])) {
 
         $temp = $_[0];
-        $temp	=~ s/\*//g;			# Remove asterisks 
+        $temp	=~ s/\*//g;			# Remove asterisks
         $temp =~ s/\s{2,}/ /g;		# Remove any multiple spaces
         $temp =~ s/by (majority|plurality) vote$//i;	# Delete certain phrases
         $temp =~ s/ as .*Director//;
@@ -41,7 +41,7 @@ $CODE$
         $temp =~ s/Require Majority Vote to Elect Directors in an Uncontested Election//i;
         $temp =~ s/Elect Director ohn\s+/Elect Director John /;
 
-        # Pull out text after "Elect director"; 
+        # Pull out text after "Elect director";
         # if the word "and" appears, delete the observation
         # as there are multiple names.
         if(!($temp  =~ /\sand\s/i) && $temp =~ /(?:Elect.*Directors?)\s+(.+)$/i) {
@@ -49,17 +49,17 @@ $CODE$
 
             # Remove leading spaces
             $name =~ s/^\s+//;
-            
+
             if ($name =~ ",") {
-                
+
                 # If there's a comma, put the part after the first comma into a suffix
                 if ($name =~ /(.+?)\s+([\w']*?)\s?,\s?(.*)\s?$/) {
                     ($first_name, $last_name, $suffix) = ($1, $2, $3);
                 }
 
             # Some suffixes are not always separated by a comma, but we can be confident that
-            # they're suffixes. Pull these out too.    
-            } elsif ($name =~ 
+            # they're suffixes. Pull these out too.
+            } elsif ($name =~
                     /^(.*)\s+(.*?)\s+(JR\s?\.?|PH\.?D\.?|II|III|IV|V|VI|M\.?D\.?|\(RET(\.|ired)?\)|3D|CBE)$/i) {
                     ($first_name, $last_name, $suffix) =  ($1, $2, $3);
             } else {
@@ -72,21 +72,21 @@ $CODE$
             }
 
             # Pull out prefixes like Mr, Dr, etc.
-            if ($first_name =~ 
+            if ($first_name =~
                 /^((?:Amb\.|Ambassador|(?:Rear|Vice )?(?:Adm\.|Admiral)|RADM|(?:Maj\. |Major )?Gen\.)\.? )?(.*)$/i) {
                     ($prefix, $first_name) = ($1, $2);
             }
-            if ($prefix eq "" & $first_name =~ /^((?:(?:Lieutenant |Major )?General)\.? )?(.*)$/i) {	
+            if ($prefix eq "" & $first_name =~ /^((?:(?:Lieutenant |Major )?General)\.? )?(.*)$/i) {
                 ($prefix, $first_name) = ($1, $2);
-            }			
-            if ($prefix eq "" & $first_name =~ 
-                /^((?:Lt Gen|Hon\.|Prof\.|Professor|Rev\.|Rt\. Hon\.?|Sir|Dr|Mr|Mrs|Ms)\.? )?(.*)$/i) { 
+            }
+            if ($prefix eq "" & $first_name =~
+                /^((?:Lt Gen|Hon\.|Prof\.|Professor|Rev\.|Rt\. Hon\.?|Sir|Dr|Mr|Mrs|Ms)\.? )?(.*)$/i) {
                     ($prefix, $first_name) = ($1, $2);
             }
-            if ($prefix eq "" & $first_name =~ /^(Sen\. |Senator )?(.*)$/i) { 
+            if ($prefix eq "" & $first_name =~ /^(Sen\. |Senator )?(.*)$/i) {
                 ($prefix, $first_name) = ($1, $2);
             }
-        } 
+        }
 
     }
 
@@ -97,8 +97,8 @@ $CODE$
         $last_name_prefix =~ s/^\s+//;
         $last_name = $last_name_prefix . ' ' . $last_name;
     }
-    
-    return {first_name => $first_name, middle_initial => $middle_initial, 
+
+    return {first_name => $first_name, middle_initial => $middle_initial,
             last_name => $last_name, suffix => $suffix, prefix => $prefix };
 $CODE$ LANGUAGE plperl;
 
@@ -108,8 +108,8 @@ SET work_mem='3GB';
 
 DROP TABLE IF EXISTS issvoting.auto_names;
 
-CREATE TABLE issvoting.auto_names AS 
-SELECT itemdesc, meetingdate, issvoting.extract_name(itemdesc) AS name
+CREATE TABLE issvoting.auto_names AS
+SELECT DISTINCT itemdesc, issvoting.extract_name(itemdesc) AS name
 FROM issvoting.compvote
 WHERE itemdesc ~* '^\s*Elect';
 
@@ -117,10 +117,10 @@ ALTER TABLE issvoting.auto_names OWNER TO activism;
 
 DROP TABLE IF EXISTS issvoting.director_names;
 
-CREATE TABLE issvoting.director_names AS 
+CREATE TABLE issvoting.director_names AS
 SELECT itemdesc, prefix, first_name, middle_initial, last_name, suffix
 FROM issvoting.manual_names
-UNION 
+UNION
 SELECT itemdesc, (name).prefix, (name).first_name, (name).middle_initial,
     (name).last_name, (name).suffix
 FROM issvoting.auto_names
