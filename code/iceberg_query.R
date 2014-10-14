ee5c786f2150651940d93e0ae4ef41a5d54c8725
Ian Gow <igow@hbs.edu>
2014-10-14 12:27:30 -0400

    Add query to examine stocks in portfolio of an activist.
    - Initial focus on Starboard
    - Purpose of query is, in part, to answer Doug Skinner's question about 13D filings potentially being the tip of the iceberg.

---
+++ code/iceberg_query.R
@@ -0,0 +1,40 @@
+library(RPostgreSQL)
+pg <- dbConnect(PostgreSQL())
+
+temp <- dbGetQuery(pg, "
+    WITH sharkwatch_permno AS (
+        SELECT DISTINCT a.*, b.permno
+        FROM factset.sharkwatch AS a
+        INNER JOIN crsp.stocknames AS b
+        ON substr(a.cusip_9_digit, 1, 8)=b.ncusip),
+
+    stocknames AS (
+        SELECT permno, array_agg(comnam) AS crsp_co_names
+        FROM crsp.stocknames
+        GROUP BY permno),
+
+    starboard_data AS (
+        SELECT permno, d.cusip_number, min(c.period_of_report), max(c.period_of_report),
+            bool_or(f.permno IS NOT NULL) AS on_sharkwatch
+        FROM activist_director.activist_cik_arrays AS a
+        INNER JOIN whalewisdom.filers AS b
+        ON b.cik=ANY(a.ciks)
+        INNER JOIN whalewisdom.filings AS c
+        USING (filer_id)
+        INNER JOIN whalewisdom.filing_stock_records AS d
+        USING (filing_id)
+        LEFT JOIN crsp.stocknames AS e
+        ON substr(d.cusip_number,1,8)=e.ncusip
+        LEFT JOIN sharkwatch_permno AS f
+        USING (permno)
+        WHERE b.cik=1517137 --'Starboard Value LP'=ANY(activist_names)
+        GROUP BY permno, d.cusip_number)
+
+    SELECT * -- on_sharkwatch, count(*)
+    FROM starboard_data
+    LEFT JOIN stocknames
+    USING (permno)")
+
+write.csv(temp, "~/Google Drive/activism/data/starboard_stocks.csv", row.names=FALSE)
+
+rs <- dbDisconnect(pg)
