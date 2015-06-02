0fcac499a7c9cba2523ecfbb9f4d57672a097498
Ian Gow <igow@hbs.edu>
2015-06-02 17:49:53 -0400

    Created code to analyze problematic board demand cases.

--- code/analyze_demand_dates.R
+++ code/analyze_demand_dates.R
@@ -1,57 +1,31 @@
-library(tidyr)
+# library(tidyr)
 library(dplyr)
-
-set_to_indicator <- function(df, var) {
-
-    df %>%
-        mutate_(indicator = ~TRUE) %>%
-        spread_(var, "indicator", fill=FALSE)
-}
-
-cor_ind <- function(df) {
-    cor(df[, unlist(lapply(df, is.logical))])
-}
 
 pg <- src_postgres()
 
-key_dates <- tbl(pg, dplyr::sql("
+key_dates <- tbl(pg, sql("
     SELECT campaign_id, demand_date, UNNEST(demand_types) AS demand_type
     FROM activist_director.key_dates"))
 
-activism_events <- tbl(pg, dplyr::sql("
-    SELECT *
+activism_events <- tbl(pg, sql("
+    SELECT campaign_id, synopsis_text, activist_demand,
+        dissident_board_seats_sought
     FROM activist_director.activism_events"))
 
-event_tbl <- left_join(activism_events, key_dates) #, by="campaign_id")
+# Identify the different kinds of demands we've coded
+key_dates %>% group_by(demand_type) %>%
+    summarize(count = n()) %>%
+    arrange(desc(count)) %>%
+    collect()
 
-
-first_board_demands <- key_dates %>%
+# Identify cases with board but not board demand dates
+key_dates %>%
     filter(demand_type=="board") %>%
     group_by(campaign_id) %>%
-    summarize(first_board_date = min(demand_date))
+    summarize(first_board_date = min(demand_date)) %>%
+    left_join(activism_events, .) %>%
+    mutate(board_demand=!is.na(first_board_date)) %>%
+    filter(is.na(first_board_date) & activist_demand) %>%
+    collect() %>% as.data.frame -> problem_cases
 
-first_demands <- key_dates %>%
-    group_by(campaign_id) %>%
-    summarize(first_board_date = min(demand_date))
 
-first_demands_merged <- left_join(first_demands, first_board_demands)
-
-event_tbl <- left_join(activism_events, first_demands_merged)
-
-event_tbl %>%
-    select(campaign_id, demand_type) %>%
-    distinct %>%
-    as.data.frame %>%
-    mutate_(indicator = ~TRUE) %>%
-    spread(demand_type, indicator, fill=FALSE) %>%
-    cor_ind %>%
-    print
-
-event_tbl %>%
-    select(campaign_id, demand_date, demand_type) %>%
-    distinct %>%
-    as.data.frame %>%
-    mutate_(indicator = ~TRUE) %>%
-    spread(demand_type, indicator, fill=FALSE) %>%
-    cor_ind %>%
-    print
