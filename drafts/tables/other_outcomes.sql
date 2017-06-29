ff3ab2f1590ac75404997db35362ed258119a29b
Sa-Pyung Shin <sshin@hbs.edu>
2017-06-29 05:24:54 -0400

    Adding _p1 (t+1) to outcome data

--- drafts/tables/other_outcomes.sql
+++ drafts/tables/other_outcomes.sql
@@ -44,11 +44,23 @@ compustat_w_lags AS (
     SELECT gvkey, datadate, at, che, dvc, prstkc, pstkrv, oibdp,
         dltt, dlc, ceq, xrd, xad, capx,
         lag(at, 1) OVER w AS at_m1,
+        lead(at, 1) OVER w AS at_p1,
+        lead(che, 1) OVER w AS che_p1,
+        lead(ceq, 1) OVER w AS ceq_p1,
+        lead(dlc, 1) OVER w AS dlc_p1,
+        lead(dltt, 1) OVER w AS dltt_p1,
+        sum(oibdp) OVER p1 AS oibdp_cum_p1,
+        sum(coalesce(dvc,0)) OVER p1 AS dvc_cum_p1,
+        sum(coalesce(prstkc,0)) OVER p1 AS prstkc_cum_p1,
+        sum(coalesce(pstkrv,0)) OVER p1 AS pstkrv_cum_p1,
+        sum(xrd) OVER p1 AS xrd_cum_p1,
+        sum(xad) OVER p1 AS xad_cum_p1,
+        sum(capx) OVER p1 AS capx_cum_p1,
         lead(at, 2) OVER w AS at_p2,
         lead(che, 2) OVER w AS che_p2,
-        lead(ceq) OVER w AS ceq_p2,
-        lead(dlc) OVER w AS dlc_p2,
-        lead(dltt) OVER w AS dltt_p2,
+        lead(ceq, 2) OVER w AS ceq_p2,
+        lead(dlc, 2) OVER w AS dlc_p2,
+        lead(dltt, 2) OVER w AS dltt_p2,
         sum(oibdp) OVER p2 AS oibdp_cum_p2,
         sum(coalesce(dvc,0)) OVER p2 AS dvc_cum_p2,
         sum(coalesce(prstkc,0)) OVER p2 AS prstkc_cum_p2,
@@ -58,9 +70,9 @@ compustat_w_lags AS (
         sum(capx) OVER p2 AS capx_cum_p2,
         lead(at, 3) OVER w AS at_p3,
         lead(che, 3) OVER w AS che_p3,
-        lead(ceq) OVER w AS ceq_p3,
-        lead(dlc) OVER w AS dlc_p3,
-        lead(dltt) OVER w AS dltt_p3,
+        lead(ceq, 3) OVER w AS ceq_p3,
+        lead(dlc, 3) OVER w AS dlc_p3,
+        lead(dltt, 3) OVER w AS dltt_p3,
         sum(oibdp) OVER p3 AS oibdp_cum_p3,
         sum(coalesce(dvc,0)) OVER p3 AS dvc_cum_p3,
         sum(coalesce(prstkc,0)) OVER p3 AS prstkc_cum_p3,
@@ -71,6 +83,8 @@ compustat_w_lags AS (
         lag(datadate) OVER w AS datadate_m1
     FROM compustat
     WINDOW w AS (PARTITION BY gvkey ORDER BY datadate),
+        p1 AS (PARTITION BY gvkey ORDER BY datadate
+                ROWS BETWEEN 1 FOLLOWING AND 1 FOLLOWING),
         p2 AS (PARTITION BY gvkey ORDER BY datadate
                 ROWS BETWEEN 1 FOLLOWING AND 2 FOLLOWING),
         p3 AS (PARTITION BY gvkey ORDER BY datadate
@@ -82,6 +96,18 @@ outcomes AS (
         CASE WHEN at > 0 THEN xrd/at END AS rnd_cum,
         CASE WHEN at > 0 THEN xad/at END AS adv_cum,
         CASE WHEN at > 0 THEN capx/at END AS capex_cum,
+
+        CASE WHEN at > 0 THEN che_p1/at END AS cash_p1,
+        CASE WHEN oibdp_cum_p1 > 0
+            THEN (dvc_cum_p1 + prstkc_cum_p1 - pstkrv_cum_p1)/oibdp_cum_p1 END AS payout_p1,
+        -- CASE WHEN dltt+dlc+ceq > 0 THEN (dltt+dlc)/(dltt+dlc+ceq) END AS lev,
+        CASE WHEN dltt_p1 + dlc_p1 + ceq_p1 > 0
+            THEN (dltt_p1 + dlc_p1)/(dltt_p1 + dlc_p1 + ceq_p1)
+        END AS leverage_p1,
+        CASE WHEN at > 0 THEN (xrd_cum_p1)/at END AS rnd_cum_p1,
+        CASE WHEN at > 0 THEN (xad_cum_p1)/at END AS adv_cum_p1,
+        CASE WHEN at > 0 THEN (capx_cum_p1)/at END AS capex_cum_p1,
+
         CASE WHEN at > 0 THEN che_p2/at END AS cash_p2,
         CASE WHEN oibdp_cum_p2 > 0
             THEN (dvc_cum_p2 + prstkc_cum_p2 - pstkrv_cum_p2)/oibdp_cum_p2 END AS payout_p2,
@@ -92,6 +118,7 @@ outcomes AS (
         CASE WHEN at > 0 THEN (xrd_cum_p2)/at END AS rnd_cum_p2,
         CASE WHEN at > 0 THEN (xad_cum_p2)/at END AS adv_cum_p2,
         CASE WHEN at > 0 THEN (capx_cum_p2)/at END AS capex_cum_p2,
+
         CASE WHEN at > 0 THEN che_p3/at END AS cash_p3,
         CASE WHEN oibdp_cum_p3 > 0
             THEN (dvc_cum_p3 + prstkc_cum_p3 - pstkrv_cum_p3)/oibdp_cum_p3 END AS payout_p3,
@@ -105,7 +132,9 @@ outcomes AS (
     FROM compustat_w_lags)
 
 SELECT DISTINCT a.*, rnd_cum, adv_cum, capex_cum,
+		cash_p1, payout_p1, leverage_p1, rnd_cum_p1, adv_cum_p1, capex_cum_p1,
 		cash_p2, payout_p2, leverage_p2, rnd_cum_p2, adv_cum_p2, capex_cum_p2,
+		cash_p3, payout_p3, leverage_p3, rnd_cum_p3, adv_cum_p3, capex_cum_p3,
 		b.ad_on_board
 FROM activist_director.outcome_controls AS a
 INNER JOIN outcomes AS h
