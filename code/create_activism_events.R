63e7bc6a97a5707a93625b7c5102555d3bc71f8f
Ian Gow <iandgow@gmail.com>
2017-11-13 18:24:28 +1100

    Migrated activism events code to dplyr.

--- code/create_activism_events.R
+++ code/create_activism_events.R
@@ -1,79 +1,69 @@
+library(dplyr, warn.conflicts = FALSE)
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
 
-# Now run SQL to get the other data (word counts, word share, lexical diversity)
-sql <- paste(readLines("code/create_activism_events.sql"),
-             collapse="\n")
-rs <- dbGetQuery(pg, sql)
+dbGetQuery(pg, "SET work_mem='8GB'")
+
+dbGetQuery(pg, "SET search_path='activist_director'")
+dbGetQuery(pg, "DROP TABLE IF EXISTS activism_events_new")
+
+activism_sample <- tbl(pg, sql("SELECT * FROM activism_sample"))
+
+activist_directors <- tbl(pg, sql("SELECT * FROM activist_directors"))
+
+activist_director <-
+    activist_directors %>%
+    group_by(permno, dissident_group, eff_announce_date) %>%
+    summarize(
+        first_appointment_date= min(appointment_date),
+        num_activist_directors = n(),
+        num_affiliate_directors = sum(as.integer(!independent)),
+        num_unaffiliate_directors = sum(as.integer(independent)))
+
+matched <-
+    activism_sample %>%
+    left_join(activist_director,
+              by = c("permno", "eff_announce_date", "dissident_group")) %>%
+    mutate(activist_director = !is.na(dissident_board_seats_wongranted_date) |
+               dissident_board_seats_won > 0 |
+               campaign_resulted_in_board_seats_for_activist) %>%
+    mutate(activist_director = coalesce(activist_director, FALSE)) %>%
+    mutate(elected = proxy_fight_went_the_distance=='Yes' & activist_director) %>%
+    mutate(activist_demand = activist_demand_old |
+            !is.na(first_board_demand_date)) %>%
+    mutate(category = case_when(
+            activist_director ~ 'activist_director',
+            activist_demand ~ 'activist_demand',
+            activism ~ 'activism',
+	        TRUE ~ '_none')) %>%
+    mutate(affiliated = case_when(
+            activist_director & num_affiliate_directors > 0 ~ 'affiliated',
+            activist_director & num_affiliate_directors == 0 ~ 'non_affiliated',
+	        TRUE ~ category),
+        two_plus = case_when(
+            num_activist_directors > 1 ~ 'two_plus_directors',
+	        activist_director ~ 'one_director',
+            TRUE ~ category),
+         early = case_when(
+             first_appointment_date - eff_announce_date <= 180 ~ 'early',
+             first_appointment_date - eff_announce_date > 180 ~ 'late',
+             TRUE ~ category),
+        big_investment = case_when(
+	         activist_director &
+                market_capitalization_at_time_of_campaign *
+                dissident_group_ownership_percent_at_announcement/100
+	                > 100 ~ 'big investment director',
+             activist_director ~ 'small investment director',
+	         TRUE ~ category)) %>%
+    compute(name = "activism_events_new", temporary = FALSE)
 
 sql <- paste0("
-    ALTER TABLE activist_director.activism_events OWNER TO activism;
 
-    COMMENT ON TABLE activist_director.activism_events IS
+    ALTER TABLE activism_events_new OWNER TO activism;
+
+    COMMENT ON TABLE activism_events_new IS
         'CREATED USING create_activism_events.R ON ", Sys.time() , "';")
 
 rs <- dbGetQuery(pg, sql)
 
-# Get corrected data on board-related activism from Google Sheets document ----
-library(googlesheets)
-
-# As a one-time thing per user and machine, you will need to run gs_auth()
-# to authorize googlesheets to access your Google Sheets.
-gs <- gs_key("16Hmw7B1kzL5eIa3k7Jw5j-9RTsfspEB18r-wNfTRiYM")
-
-event_fix <- gs_read(gs)
-
-# Put data into PostgreSQL ----
-
-rs <- dbWriteTable(pg, name=c("activist_director", "event_fix"), event_fix,
-                   overwrite=TRUE, row.names=FALSE)
-sql <- paste0("
-    ALTER TABLE activist_director.event_fix OWNER TO activism;
-
-    COMMENT ON TABLE activist_director.event_fix IS
-        'CREATED USING create_activism_events.R ON ", Sys.time() , "';")
-
-rs <- dbGetQuery(pg, sql)
-
-cat("Original data:")
-targeted_firms_mod <- dbGetQuery(pg, "
-    SELECT *
-    FROM activist_director.activism_events")
-
-table(targeted_firms_mod$board_related, targeted_firms_mod$proxy_fight)
-
-# Update relevant table ----
-
-# TODO: Sean, we are implicitly using PERMNO and first_date as the key here
-#      I guess we should make sure that there are no cases where there
-#      are multiple dissident groups launching activism against a single firm
-#      on the same date.
-#      Also, are there other fields that need to be updated?
-#      Fields that may need to be updated:
-#          primary_campaign_type, targeted_firm_board_settled, activism_type,
-#          non_proxy_board, proxy_fight
-
-# holder_type on_defc went_the_distance dissident_group_ownership_percent
-
-rs <- dbGetQuery(pg, "
-    UPDATE activist_director.activism_events AS a
-    SET board_related=b.board_related
-    FROM activist_director.event_fix AS b
-    WHERE b.campaign_id=ANY(a.campaign_ids);
-
-    UPDATE activist_director.activism_events AS a
-    SET proxy_fight=b.proxy_fight
-    FROM activist_director.event_fix AS b
-    WHERE b.campaign_id=ANY(a.campaign_ids);
-
-    DROP TABLE activist_director.event_fix;")
-
-# Check what the effect is of the changes above ----
-cat("Modified data:")
-targeted_firms_mod <- dbGetQuery(pg, "
-    SELECT *
-    FROM activist_director.activism_events")
-
-table(targeted_firms_mod$board_related, targeted_firms_mod$proxy_fight)
-
-rs <-dbDisconnect(pg)
+dbDisconnect(pg)
