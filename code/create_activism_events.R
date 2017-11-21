fb84dd79a202f761cd1e42f14cfe5e440e3f39fb
Sa-Pyung Shin <sshin@hbs.edu>
2017-11-21 02:15:43 -0500

    "_new" removed for activist_director.activism_events

--- code/create_activism_events.R
+++ code/create_activism_events.R
@@ -5,7 +5,7 @@ pg <- dbConnect(PostgreSQL())
 dbGetQuery(pg, "SET work_mem='8GB'")
 
 dbGetQuery(pg, "SET search_path='activist_director'")
-dbGetQuery(pg, "DROP TABLE IF EXISTS activism_events_new")
+dbGetQuery(pg, "DROP TABLE IF EXISTS activism_events")
 
 activism_sample <- tbl(pg, sql("SELECT * FROM activism_sample"))
 
@@ -40,28 +40,28 @@ matched <-
             activist_director & num_affiliate_directors > 0 ~ 'affiliated',
             activist_director & num_affiliate_directors == 0 ~ 'non_affiliated',
 	        TRUE ~ category),
-        two_plus = case_when(
+           two_plus = case_when(
             num_activist_directors > 1 ~ 'two_plus_directors',
 	        activist_director ~ 'one_director',
             TRUE ~ category),
-         early = case_when(
-             first_appointment_date - eff_announce_date <= 180 ~ 'early',
-             first_appointment_date - eff_announce_date > 180 ~ 'late',
-             TRUE ~ category),
-        big_investment = case_when(
-	         activist_director &
+           early = case_when(
+            first_appointment_date - eff_announce_date <= 180 ~ 'early',
+            first_appointment_date - eff_announce_date > 180 ~ 'late',
+            TRUE ~ category),
+           big_investment = case_when(
+	        activist_director &
                 market_capitalization_at_time_of_campaign *
                 dissident_group_ownership_percent_at_announcement/100
 	                > 100 ~ 'big investment director',
-             activist_director ~ 'small investment director',
-	         TRUE ~ category)) %>%
-    compute(name = "activism_events_new", temporary = FALSE)
+            activist_director ~ 'small investment director',
+	        TRUE ~ category)) %>%
+    compute(name = "activism_events", temporary = FALSE)
 
 sql <- paste0("
 
-    ALTER TABLE activism_events_new OWNER TO activism;
+    ALTER TABLE activism_events OWNER TO activism;
 
-    COMMENT ON TABLE activism_events_new IS
+    COMMENT ON TABLE activism_events IS
         'CREATED USING create_activism_events.R ON ", Sys.time() , "';")
 
 rs <- dbGetQuery(pg, sql)
