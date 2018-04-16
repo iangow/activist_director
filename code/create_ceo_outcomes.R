c120652970b51dc7c12c6a371e52a41757a71265
Ian Gow <igow@hbs.edu>
2018-04-16 17:38:08 -0400

    Consolidated CEO comp and turnover into one file.
    - Relates to #42.

--- code/create_ceo_outcomes.R
+++ code/create_ceo_outcomes.R
@@ -5,8 +5,6 @@ pg <- dbConnect(PostgreSQL())
 rs <- dbExecute(pg, "SET search_path TO activist_director")
 
 # Get data from activist director tables ----
-ceo_turnover <- tbl(pg, "ceo_turnover")
-ceo_comp <- tbl(pg, "ceo_comp")
 outcome_controls <- tbl(pg, "outcome_controls")
 
 # Data from CRSP ----
@@ -14,6 +12,105 @@ ccmxpf_linktable <-
     tbl(pg, sql("SELECT * FROM crsp.ccmxpf_linktable"))
 
 gvkeys <- tbl(pg, sql("SELECT * FROM executive.gvkeys"))
+
+# Data from Equilar ----
+proxy_company <- tbl(pg, sql("SELECT * FROM executive.proxy_company"))
+proxy_management <-
+    tbl(pg, sql("SELECT * FROM executive.proxy_management"))
+proxy_management_calc <-
+    tbl(pg, sql("SELECT * FROM executive.proxy_management_calc"))
+executive <-
+    tbl(pg, sql("SELECT * FROM executive.executive"))
+
+fy_ends <-
+    proxy_company %>%
+    select(company_id, fy_id, fy_end)
+
+# CEO compensation data ----
+executive_names <-
+    executive %>%
+    select(executive_id, lname, fname)
+
+salary <-
+    proxy_management %>%
+    select(fy_id, company_id, management_id, fiscal_year,
+           executive_id, comp_salary, title)
+
+total_comp <-
+    proxy_management_calc %>%
+    filter(is_ceo | is_ceo_iss) %>%
+    select(fy_id, company_id, management_id, comp_total)
+
+combined_data <-
+    salary %>%
+    inner_join(executive_names) %>%
+    inner_join(total_comp) %>%
+    inner_join(fy_ends) %>%
+    select(-fy_id) %>%
+    mutate(perf_comp = if_else(comp_total > 0, 1 - 1.0*comp_salary/comp_total, NA_real_))
+
+ceo_comp <-
+    combined_data %>%
+    group_by(company_id, fy_end) %>%
+    summarize(ceo_comp = avg(comp_total),
+              perf_comp = avg(perf_comp)) %>%
+    group_by(company_id) %>%
+    arrange(fy_end) %>%
+    mutate(ceo_comp_p1 = lead(ceo_comp, 1L),
+           ceo_comp_p2 = lead(ceo_comp, 2L),
+           ceo_comp_p3 = lead(ceo_comp, 3L),
+           perf_comp_p1 = lead(perf_comp, 1L),
+           perf_comp_p2 = lead(perf_comp, 2L),
+           perf_comp_p3 = lead(perf_comp, 3L)) %>%
+    mutate(ceo_comp = if_else(ceo_comp > 0, ln(ceo_comp), NA_real_),
+           ceo_comp_p1 = if_else(ceo_comp_p1 > 0, ln(ceo_comp_p1), NA_real_),
+           ceo_comp_p2 = if_else(ceo_comp_p2 > 0, ln(ceo_comp_p2), NA_real_),
+           ceo_comp_p3 = if_else(ceo_comp_p3 > 0, ln(ceo_comp_p3), NA_real_)) %>%
+    compute()
+
+# CEO turnover ----
+ceos <-
+    proxy_management_calc %>%
+    filter(is_ceo) %>%
+    select(company_id, fy_id, management_id) %>%
+    inner_join(proxy_management) %>%
+    inner_join(fy_ends) %>%
+    filter(is.na(date_resign_ceo) | date_resign_ceo > fy_end) %>%
+    select(company_id, fy_end, management_id, executive_id, date_start_ceo,
+           date_resign_ceo, title) %>%
+    compute()
+
+num_ceos <-
+    ceos %>%
+    select(company_id, fy_end, executive_id) %>%
+    distinct() %>%
+    group_by(company_id, fy_end) %>%
+    summarize(num_ceos = n()) %>%
+    ungroup() %>%
+    compute()
+
+sole_ceo_firm_years <-
+    num_ceos %>%
+    filter(num_ceos == 1)
+
+# Finalize table creation
+ceo_turnover <-
+    ceos %>%
+    semi_join(sole_ceo_firm_years) %>%
+    group_by(company_id) %>%
+    arrange(fy_end) %>%
+    mutate(lag_fy_end = lag(fy_end),
+           ceo_turnover = executive_id != lag(executive_id)) %>%
+    ungroup() %>%
+    filter(is.na(lag_fy_end) | lag_fy_end > sql("fy_end - interval '13 months'")) %>%
+    select(company_id, fy_end, ceo_turnover) %>%
+    group_by(company_id) %>%
+    arrange(fy_end) %>%
+    mutate(ceo_turnover_p1 = lead(ceo_turnover, 1L)) %>%
+    mutate(ceo_turnover_p2 = ceo_turnover_p1 | lead(ceo_turnover, 2L)) %>%
+    mutate(ceo_turnover_p3 = ceo_turnover_p2 | lead(ceo_turnover, 3L)) %>%
+    mutate_at(vars(matches("^ceo_turnover_p")), .funs = as.integer) %>%
+    compute()
 
 # Process data ----
 ceo_data <-
