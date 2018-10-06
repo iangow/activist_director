8863a229ebf56b04e1ddc5f1126449bba4043b9a
Ian Gow <iandgow@gmail.com>
2018-10-06 18:44:29 -0400

    Minor code clean-up.

--- code/create_activist_director_years.R
+++ code/create_activist_director_years.R
@@ -4,16 +4,13 @@ pg <- dbConnect(PostgreSQL())
 
 dbGetQuery(pg, "SET work_mem='8GB'")
 
-dbGetQuery(pg, "SET search_path='activist_director'")
-dbGetQuery(pg, "DROP TABLE IF EXISTS activist_director_years_new")
+dbGetQuery(pg, "SET search_path TO activist_director")
+
+activist_directors <- tbl(pg, "activist_directors")
+outcome_controls <- tbl(pg, "outcome_controls")
 
 comp.funda <- tbl(pg, sql("SELECT * FROM comp.funda"))
 crsp.ccmxpf_linktable <- tbl(pg, sql("SELECT * FROM crsp.ccmxpf_linktable"))
-activist_directors <- tbl(pg, sql("SELECT * FROM activist_directors"))
-outcome_controls <- tbl(pg, sql("SELECT * FROM outcome_controls"))
-# DROP TABLE IF EXISTS activist_director.activist_director_years;
-
-# CREATE TABLE activist_director.activist_director_years AS
 
 # Compustat with PERMNO
 firm_years <-
@@ -32,28 +29,36 @@ firm_years <-
 activist_director <-
     activist_directors %>%
     group_by(campaign_id, permno) %>%
-    summarize(appointment_date = min(appointment_date),
-              retirement_date = max(coalesce(retirement_date, '2016-12-31')))
+    summarize(appointment_date = min(appointment_date, na.rm = TRUE),
+              retirement_date = max(coalesce(retirement_date, '2017-12-31'), na.rm = TRUE)) %>%
+    ungroup() %>%
+    compute()
+
+matched <-
+    outcome_controls %>%
+    inner_join(activist_director, by="permno") %>%
+    filter(between(datadate, appointment_date, retirement_date)) %>%
+    mutate(on_board = !is.na(permno)) %>%
+    distinct(permno, datadate, on_board)
 
 activist_director_on_board <-
     outcome_controls %>%
-    left_join(activist_director, by="permno") %>%
-    filter(between(datadate, appointment_date, retirement_date)) %>%
-    mutate(on_board = !is.na(permno)) %>%
-    select(permno, datadate, on_board)
+    left_join(matched, by = c("permno", "datadate")) %>%
+    mutate(on_board = coalesce(on_board, FALSE)) %>%
+    distinct(permno, datadate, on_board)
 
-dbGetQuery(pg, "DROP TABLE IF EXISTS activist_director_years")
+rs <- dbExecute(pg, "DROP TABLE IF EXISTS activist_director_years_new")
 
 activist_director_years <-
     activist_director_on_board %>%
     group_by(permno, datadate) %>%
     summarize(ad_on_board = bool_or(on_board)) %>%
     arrange(permno, datadate) %>%
-    compute(name = "activist_director_years", temporary=FALSE)
+    compute(name = "activist_director_years_new", temporary=FALSE)
 
-dbGetQuery(pg, "COMMENT ON TABLE activist_director_years IS
+rs <- dbExecute(pg, "COMMENT ON TABLE activist_director_years_new IS
                 'CREATED USING create_activist_director_years.R'")
 
-dbGetQuery(pg, "ALTER TABLE activist_director_years OWNER TO activism")
+rs <- dbExecute(pg, "ALTER TABLE activist_director_years_new OWNER TO activism")
 
 dbDisconnect(pg)
