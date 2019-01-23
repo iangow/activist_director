ed92f9bf6def1fc8410672f1c5a5a1c99f23aede
Ian Gow <iandgow@gmail.com>
2019-01-22 19:53:03 -0500

    Added "recent campaigns" variable to data.
    - Relates to #91.

--- code/create_prior_campaigns.R
+++ code/create_prior_campaigns.R
@@ -10,15 +10,33 @@ rs <- dbExecute(pg, "SET search_path TO factset")
 sharkwatch <- tbl(pg, "sharkwatch")
 dissidents <- tbl(pg, "dissidents")
 
-dbExecute(pg, "DROP TABLE IF EXISTS prior_campaigns")
-prior_campaigns <-
+campaigns <-
     sharkwatch %>%
     inner_join(dissidents) %>%
     select(dissident, campaign_id, announce_date) %>%
-    distinct() %>%
+    distinct()
+
+recent_campaigns <-
+    campaigns %>%
+    inner_join(campaigns, by = "dissident") %>%
+    filter(between(announce_date.y,
+                   announce_date.x - sql("interval '1 year'"),
+                   announce_date.x - sql("interval '1 day'"))) %>%
+    group_by(dissident, announce_date.x) %>%
+    summarize(recent_campaigns = n()) %>%
+    rename(announce_date = announce_date.x)
+
+all_campaigns <-
+    campaigns %>%
     group_by(dissident) %>%
     arrange(announce_date) %>%
-    mutate(prev_campaigns = row_number() - 1L) %>%
+    mutate(prev_campaigns = row_number() - 1L)
+
+rs <- dbExecute(pg, "DROP TABLE IF EXISTS prior_campaigns")
+
+prior_campaigns <-
+    all_campaigns %>%
+    inner_join(recent_campaigns, by = c("dissident", "announce_date")) %>%
     compute(name = "prior_campaigns", temporary = FALSE,
             index = "dissident")
 
