05ce37aaa34b1cd73bc3e3c3b4524649c9b8fc20
sshin <mail@ewenharrison.com>
2020-09-02 05:05:17 -0400

    Sean's version of `create_ind_exp` #103

--- code/create_ind_exp.R
+++ code/create_ind_exp.R
@@ -1,83 +1,80 @@
-equilar_lib <- "executive_gsb"
+library(RPostgreSQL)
 library(dplyr, warn.conflicts = FALSE)
-library(DBI)
+library(tidyverse)
+pg <- dbConnect(PostgreSQL())
 
-pg <- dbConnect(RPostgres::Postgres(), bigint = "integer")
-
-rs <- dbExecute(pg,
-                paste0("SET search_path TO activist_director, ", equilar_lib))
-
-activist_directors <- tbl(pg, "activist_directors")
-
-activist_directors
-activist_director_equilar <- tbl(pg, "activist_director_equilar")
-
-proxy_company  <- tbl(pg, "proxy_company")
-
-fy_ends <-
-    proxy_company %>%
-    select(fy_id, company_id, fy_end)
-
-fy_ends
-proxy_company <- tbl(pg, "proxy_company")
-proxy_management <- tbl(pg, "proxy_management")
-executive <- tbl(pg, "executive")
+rs <- dbGetQuery(pg, "SET work_mem='8GB'")
+rs <- dbGetQuery(pg, "SET search_path TO activist_director, equilar_hbs")
 
 stocknames <- tbl(pg, sql("SELECT * FROM crsp.stocknames"))
 
+activist_director_equilar <- tbl(pg, "activist_director_equilar") %>% collect()
+
+equilar_w_activism <- tbl(pg, "equilar_w_activism")
+
+# Bring in SIC code----
 sics <-
     stocknames %>%
-    select(ncusip, siccd) %>%
-    rename(cusip = ncusip) %>%
+    select(permno, siccd, namedt, nameenddt) %>%
+    mutate(siccd = substr(as.character(siccd), 1, 2)) %>%
+    distinct() %>%
+    arrange(permno, namedt) %>%
+    collect()
+
+# Match Equilar company_id with SIC code----
+equilar_sics <-
+    equilar_w_activism %>%
+    inner_join(sics, by = "permno") %>%
+    filter(period >= namedt, period <= nameenddt) %>%
+    select(-permno, -namedt, -nameenddt) %>%
+    distinct() %>%
+    arrange(company_id, period) %>%
+    collect()
+
+# Very first year for each executive-company ID----
+first_years <-
+    equilar_sics %>%
+    select(executive_id, company_id, siccd, date_start) %>%
+    distinct() %>%
+    arrange(executive_id, date_start)
+
+# Create a dummy "same_sic2"----
+prior_appts <-
+    first_years %>%
+    left_join(first_years, by = c("executive_id"),
+               suffix = c("_own", "_other")) %>%
+    filter(company_id_own != company_id_other,
+           date_start_other < date_start_own) %>%
+    distinct() %>%
+    mutate(same_sic2 = siccd_own==siccd_other) %>%
+    filter(date_start_own >= '2004-01-01') %>%
+    select(company_id=company_id_own, executive_id, same_sic2) %>%
+    group_by(executive_id, company_id) %>%
+    summarize(prior_ind_exp = sum(same_sic2))
+
+table(prior_appts$prior_ind_exp)
+
+# Identify activist director cases using activist_director_equilar----
+final <-
+    prior_appts %>%
+    left_join(activist_director_equilar) %>%
+    mutate(activist_director=!is.na(independent),
+           affiliated_director=ifelse(!activist_director,"_na",ifelse(!independent,"affiliated","unaffiliated"))) %>%
+    select(company_id, executive_id, activist_director, affiliated_director, prior_ind_exp) %>%
     distinct()
 
-equilar_sics <-
-    proxy_company %>%
-    select(company_id, cusip) %>%
-    inner_join(sics, by = "cusip") %>%
-    distinct() %>%
-    select(-cusip) %>%
-    compute()
+final %>% group_by(activist_director) %>% summarise(mean(prior_ind_exp))
 
-start_dates <-
-    proxy_management %>%
-    select(company_id, fy_id, executive_id, date_start) %>%
-    inner_join(fy_ends, by = c("company_id", "fy_id")) %>%
-    mutate(date = coalesce(date_start, fy_end)) %>%
-    group_by(company_id, executive_id) %>%
-    summarize(date = min(date, na.rm = TRUE)) %>%
-    ungroup() %>%
-    compute()
+final %>% group_by(affiliated_director) %>% summarise(mean(prior_ind_exp))
 
-prior_appts <-
-    activist_director_equilar %>%
-    inner_join(start_dates, by = c("executive_id"),
-              suffix = c("_own", "_other")) %>%
-    filter(company_id_own != company_id_other,
-           date < appointment_date) %>%
-    left_join(equilar_sics, by=c("company_id_own"="company_id")) %>%
-    rename(sic_own = siccd) %>%
-    left_join(equilar_sics, by=c("company_id_other"="company_id")) %>%
-    rename(sic_other = siccd) %>%
-    select(campaign_id, first_name, last_name, sic_own, sic_other)
+table(final$prior_ind_exp, final$activist_director)
 
-rs <- dbExecute(pg, "DROP TABLE IF EXISTS ind_exp")
+table(final$prior_ind_exp, final$affiliated_director)
 
-ind_exp <-
-    prior_appts %>%
-    mutate(same_sic2 = substr(as.character(sic_own), 1, 2)==
-               substr(as.character(sic_other), 1, 2)) %>%
-    group_by(campaign_id, first_name, last_name) %>%
-    summarize(prior_ind_exp = bool_or(same_sic2)) %>%
-    compute(name = "ind_exp", temporary = FALSE)
+t.test(subset(final,activist_director)$prior_ind_exp, subset(final,!activist_director)$prior_ind_exp)
 
-rs <- dbExecute(pg, "ALTER TABLE ind_exp OWNER TO activism")
+t.test(subset(final,affiliated_director=="affiliated")$prior_ind_exp, subset(final,affiliated_director=="_na")$prior_ind_exp)
 
-sql <- paste0("
-    COMMENT ON TABLE ind_exp IS
-              'CREATED USING create_ind_exp.R ON ",
-              format(Sys.time(), "%Y-%m-%d %X %Z"), "';")
+t.test(subset(final,affiliated_director=="unaffiliated")$prior_ind_exp, subset(final,affiliated_director=="_na")$prior_ind_exp)
 
-rs <- dbExecute(pg, sql)
-
-rs <- dbDisconnect(pg)
+t.test(subset(final,affiliated_director=="unaffiliated")$prior_ind_exp, subset(final,affiliated_director=="affiliated")$prior_ind_exp)
