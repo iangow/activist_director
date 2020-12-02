37ce41603f8af07036bd6683ce0f5144f65f8bd0
sshin <mail@ewenharrison.com>
2020-12-02 04:25:56 -0500

    Minor change with import_activist_director_skills.R

--- code/import_activist_director_skills.R
+++ code/import_activist_director_skills.R
@@ -10,12 +10,11 @@ names(activist_director_skills) <- tolower(names(activist_director_skills))
 
 activist_director_skills <-
     activist_director_skills %>%
-    mutate(academic = str_detect(tolower(skillset_bio), "academia|academic|dean|doctorate|education|faculty|graduate|masters|phd|ph.d|ph.d.|professor|school environment"),
+    mutate(academic = str_detect(tolower(skillset_bio), "academia|academic|dean|doctorate|education|faculty|graduate|masters|phd|ph.d|ph d|professor|school environment"),
            company_business = str_detect(tolower(skillset_bio), "all aspects of our industry|chief executive officer of our|chief executive officer of the company|company’s business|executive of our|executive of the company|experience with the company|historical insight|historical knowledge|history of the operation|history with our company|in-depth knowledge of|industry-specific perspective|industry experience|industry knowledge|inner workings|insider’s perspective|internal operation|knowledge of all aspects of the company|knowledge of the|knowledge of the history|officer of our|officer of the company|president of our|president of the company|the company’s chief|understanding of our business|working with the company"),
            compensation = str_detect(tolower(skillset_bio), "compensation"),
            entrepreneurial = str_detect(tolower(skillset_bio), "entrepreneur|entrepreneurial|entrepreneurship|evaluating business|innovative idea"),
-           finance_accounting = str_detect(tolower(skillset_bio), "accountant|accounting and|accounting experience|accounting principles|and accounting,
-auditing|banking|capital markets|capital structure|corporate finance|experience in accounting|experience in finance|expertise in finance|finance experience|finance industry|finance matters|financial accounting|financial acumen|financial background|financial experience|financial expert|financial expertise|financial field|financial foundation|financial management|financial matters|financial reporting|financial services|investment|securities|understanding of finance"),
+           finance_accounting = str_detect(tolower(skillset_bio), "accountant|accounting and|accounting experience|accounting principles|and accounting|auditing|banking|capital markets|capital structure|corporate finance|experience in accounting|experience in finance|expertise in finance|finance experience|finance industry|finance matters|financial accounting|financial acumen|financial background|financial experience|financial expert|financial expertise|financial field|financial foundation|financial management|financial matters|financial reporting|financial services|investment|securities|understanding of finance"),
            governance = str_detect(tolower(skillset_bio), "governance"),
            government_policy = str_detect(tolower(skillset_bio), "government|policy|politics|regulatory"),
            international = str_detect(tolower(skillset_bio), "global|international|multinational|worldwide"),
@@ -24,8 +23,7 @@ auditing|banking|capital markets|capital structure|corporate finance|experience 
            management = str_detect(tolower(skillset_bio), "experience in leading|experience in managing|management"),
            manufacturing = str_detect(tolower(skillset_bio), "industrial|manufactured|manufacturing"),
            marketing = str_detect(tolower(skillset_bio), "marketing"),
-           outside_board = str_detect(tolower(skillset_bio), "board experience|board of other|board practices of other|boards of companies|boards of
-other|boards of several other|boards of various|director of other|director of several other|member of the board of|numerous boards|on the boards of|other company boards|prior service as a director|several corporate boards|several other corporate boards|varied boards"),
+           outside_board = str_detect(tolower(skillset_bio), "board experience|board of other|board practices of other|boards of companies|boards of other|boards of several other|boards of various|director of other|director of several other|member of the board of|numerous boards|on the boards of|other company boards|prior service as a director|several corporate boards|several other corporate boards|varied boards"),
            outside_executive = str_detect(tolower(skillset_bio), "as the chairman of a|business career|chief executive officer of a|executive experience|experience as a chief|experience as an executive officer of|experience as a senior|former executive of a|officer of a public|officer of other|officer of several companies|officer of numerous companies|president of a|senior-level executive|senior executive|senior management positions|serving as the ceo of a"),
            risk_management = str_detect(tolower(skillset_bio), "risk"),
            scientific = str_detect(tolower(skillset_bio), "research and development|scientific expertise"),
