8cd06eae8eab50e29eecbe057b50cdd09db481e9
Ian Gow <iandgow@gmail.com>
2023-09-27 09:09:00 +1000

    Added code to install required LaTeX packages.

---
+++ code/install_tinytex.R
@@ -0,0 +1,3 @@
+tinytex::tlmgr_install(c("harvard", "mathpazo", "courier","psnfss",
+                         "hyperref" ,"natbib", "palatino", "paralist",
+                         "parskip", "titlesec", "setspace", "pdflscape"))
